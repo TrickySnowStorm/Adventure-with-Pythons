fc06edb371a2bc6019e6a6db639dc31c9b879048
TrickySnowStorm <160676551+TrickySnowStorm@users.noreply.github.com>
2024-03-14 21:15:11 +0100

    Add files via upload

---
+++ inter_section.py
@@ -0,0 +1,17 @@
+'''
+Write a function called inter_section that takes two lists and
+finds the intersection (the elements that are present in both
+lists). The function should return a tuple of intersections. Use
+list comprehension in your solution. Use the lists below. Your
+function should return (30, 65, 80).
+list1 = [20, 30, 60, 65, 75, 80, 85]
+list2 = [ 42, 30, 80, 65, 68, 88, 95]
+'''
+
+def inter_section(list1,list2):
+    common = tuple(i for i in list1 if i in list2)
+    return common
+
+list1 = [20, 30, 60, 65, 75, 80, 85]
+list2 = [42, 30, 80, 65, 68, 88, 95]
+print(inter_section(list1,list2))
